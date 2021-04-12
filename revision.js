ebdaeb8c6ef5f84bfe38b69ae18167d7f3524f52
Volodymyr Pankiv <vpanki@softserveinc.com>
2021-04-12 09:11:31 +0300

    first commit

---
+++ revision.js
@@ -0,0 +1,21 @@
+const revision = [
+  {
+    createdDate: "2021-01-15T14:41:40.567253Z",
+    id: "6001a9a41868f64a240c3fbb",
+    name: "",
+    user: "250105",
+  },
+  {
+    createdDate: "2021-01-15T15:01:56.093302Z",
+    id: "6001ae64e7e0ef65e49e4e32",
+    name: "",
+    user: "250105",
+  },
+  {
+    createdDate: "2021-02-17T12:35:21.913899Z",
+    id: "602d0d8984de5942978152fc",
+    name: "",
+    user: "250105",
+  }
+];
+
